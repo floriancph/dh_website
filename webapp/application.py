6db66e12e5d5f0e9e2836576bf56c29e31520743
FloriVonDerGPC <florian_ries@icloud.com>
2020-06-27 01:14:18 +0200

    dbw and index

--- webapp/application.py
+++ webapp/application.py
@@ -2,20 +2,20 @@
 import json
 from flask import Flask, Response, render_template
 from webapp.flaskrun import flaskrun
-import webapp.get_key as get_key
+import webapp.db_wrapper as dbw
 
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
-def get():
-    pas = get_key.get_secret("influxdb")
-    return render_template('index.html', top_three_news=[], total_news_results=[], user=pas['user'])
-    # return Response(json.dumps({'Output': 'Moin!!'}), mimetype='application/json', status=200)
+def index():
+    top_three_news = dbw.getTopThreeNews() # Get Top Three Trends
+    total_news_results = dbw.getTotalNewsResults() # Get Total Number of News Articles (in order to calculate percentage)
+    return render_template('index.html', top_three_news=top_three_news, total_news_results=total_news_results)
 
 @application.route('/test', methods=['GET'])
 def test():
     # pas = get_key.get_secret("influxdb")
-    return Response(json.dumps({'Output': 'Moin'}), mimetype='application/json', status=200)
+    return Response(json.dumps({'Output': 'Moinsen'}), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
