24434fdfc2b0fd3f5d9fd130a832b592bfc460a0
FloriVonDerGPC <florian_ries@icloud.com>
2020-06-27 01:47:40 +0200

    a

--- webapp/application.py
+++ webapp/application.py
@@ -2,14 +2,14 @@
 import json
 from flask import Flask, Response, render_template
 from webapp.flaskrun import flaskrun
-from webapp.db_wrapper import getTopThreeNews, getTotalNewsResults
+# from webapp.db_wrapper import getTopThreeNews, getTotalNewsResults
 
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def index():
-    top_three_news = 0 # Get Top Three Trends
-    total_news_results = 0 # Get Total Number of News Articles (in order to calculate percentage)
+    top_three_news = [] # Get Top Three Trends
+    total_news_results = [] # Get Total Number of News Articles (in order to calculate percentage)
     return render_template('index.html', top_three_news=top_three_news, total_news_results=total_news_results)
 
 @application.route('/test', methods=['GET'])
