40a0f19c3e73fc77f71e82b770110fbf945f7767
FloriVonDerGPC <florian_ries@icloud.com>
2020-06-26 21:04:49 +0200

    testing boto

--- webapp/application.py
+++ webapp/application.py
@@ -2,17 +2,20 @@
 import json
 from flask import Flask, Response, render_template
 from webapp.flaskrun import flaskrun
+import get_key
 
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def get():
+    
     return render_template('index.html', top_three_news=[], total_news_results=[])
     # return Response(json.dumps({'Output': 'Moin!!'}), mimetype='application/json', status=200)
 
-@application.route('/', methods=['POST'])
-def post():
-    return Response(json.dumps({'Output': 'Moin'}), mimetype='application/json', status=200)
+@application.route('/test', methods=['GET'])
+def test():
+    pas = get_key.get_secret("influxdb")
+    return Response(json.dumps({'Output': pas['user']}), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
---
+++ webapp/get_key.py
@@ -0,0 +1,31 @@
+import boto3
+import base64
+from botocore.exceptions import ClientError
+import json
+
+def get_secret(secret_name):
+
+    region_name = "eu-central-1"
+
+    try:
+        # Create a Secrets Manager client
+        session = boto3.session.Session()
+        client = session.client(
+            service_name='secretsmanager',
+            region_name=region_name
+        )
+
+        # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+        # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        # We rethrow the exception by default.
+
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+
+        answer = json.loads(get_secret_value_response['SecretString'])
+    except:
+        answer = {'user': 'fail'}
+
+    return answer
+            
