6cce08d4f3b20da8b5b2f58bb8b1d7e9fd1e18b5
FloriVonDerGPC <florian_ries@icloud.com>
2020-06-28 01:26:44 +0200

    social changed

--- webapp/application.py
+++ webapp/application.py
@@ -12,26 +12,10 @@ def index():
     total_news_results = getTotalNewsResults() # Get Total Number of News Articles (in order to calculate percentage)
     return render_template('index.html', top_three_news=top_three_news, total_news_results=total_news_results)
 
-@application.route('/test', methods=['GET'])
-def test():
-    return Response(json.dumps({'Output': 'Moinsen'}), mimetype='application/json', status=200)
-
 @application.route('/social', methods=['GET'])
 def social():
     db_result = getSocial() # Get latest Trends (db-write within last 60mins. & sorted by no. of articles)
-    top_news = getTopNews() # Get the Top-News of today from the news.api
-    x_values = []
-    y_values = []
-
-    if len(db_result) > 0:
-        for trend in db_result:
-            x_values.append(trend['number_of_articles'])
-            y_values.append(trend['trend'])
-
-    if len(top_news) > 0:
-        top_news = top_news[0]
-
-    return render_template('social.html', db_result=db_result, top_news=top_news, x_values=x_values, y_values=y_values)
+    return render_template('social.html', db_result=db_result)
 
 if __name__ == '__main__':
     flaskrun(application)
